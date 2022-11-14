eaf92064df187ddfacaeadcccf259fae702575b9
Ilya <ilya.shuvatov1@gmail.com>
2022-11-14 10:49:14 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const temperatureInCelsius = Number(prompt('Введите температуру в градусах Цельсия'));
+
+if (temperatureInCelsius === 0) {
+    console.log(typeof temperatureInCelsius)
+    alert('0 градусов по Цельсию - это температура замерзания воды')
+}
+else if (temperatureInCelsius > 0) {
+    alert('Для замерзания воды температура должна быть 0 градусов по Цельсию либо ниже');
+}
+
+const temperatureInFahrenheit = temperatureInCelsius * 9 / 5 + 32;
+console.log(temperatureInFahrenheit)
+console.log(temperatureInCelsius)
+alert(`${temperatureInCelsius} градусов по Цельсию - это ${temperatureInFahrenheit} по Фаренгейту.`);
